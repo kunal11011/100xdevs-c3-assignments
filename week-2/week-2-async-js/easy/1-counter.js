35bdb9d2985b9025710c796357411dda44499dc7
Kunal Kothari <kunalcodes11@gmail.com>
2024-11-14 17:30:03 +0530

    - Solved 2 easy assignments from week-2-async-js.

---
+++ week-2/week-2-async-js/easy/1-counter.js
@@ -0,0 +1,6 @@
+let count = 1;
+console.log("Count is ", count);
+setInterval(() => {
+  count++;
+  console.log("Count is ", count);
+}, 1000);
---
+++ week-2/week-2-async-js/easy/2-counter.js
@@ -0,0 +1,7 @@
+let count = 1;
+function counter() {
+  console.log("Count is", count);
+  count++;
+  setTimeout(counter, 1000);
+}
+counter();
